c9f9e4045327c807198eeceeaf4e5bb678b1e385
Brahm Lower <bplower@gmail.com>
2020-09-16 23:44:15 -0700

    abstract galaxy arm tracing

--- galaxy-coord-test/coord-test.js
+++ galaxy-coord-test/coord-test.js
@@ -2,8 +2,8 @@
 let width = 1200;
 let height = 1200;
 
-let correctX = width/2;
-let correctY = height/2;
+let centerX = width/2;
+let centerY = height/2;
 
 function drawPoint(ctx, coord_x, coord_y) {
   ctx.beginPath();
@@ -15,8 +15,8 @@ function drawPoint(ctx, coord_x, coord_y) {
 
 function polarToCoords(theta, radius) {
 	// Solve for opposite
-  let coord_y = (Math.sin(theta) * radius) + correctY;
-  let coord_x = (Math.cos(theta) * radius) + correctX;
+  let coord_y = (Math.sin(theta) * radius) + centerY;
+  let coord_x = (Math.cos(theta) * radius) + centerX;
 	return [coord_x, coord_y];
 }
 
@@ -24,6 +24,14 @@ function polarToCoords(theta, radius) {
 function galaxyArmConfig({a, b, n}) {
   return (phi) => {
   	return a / Math.log(b * Math.tan(phi / (2 * n)) )
+  }
+}
+
+function traceGalaxyArm(config, callback) {
+  for (const stepNum of Array(config.numSteps).keys()) {
+    let theta = config.step * stepNum;
+    let radius = config.radius(theta) * config.scale;
+    callback(stepNum, theta, radius);
   }
 }
 
@@ -45,42 +53,41 @@ function drawGalaxy() {
   ctx.lineTo(width/2, height);
   ctx.stroke();
 
-  //let galaxyArm = galaxyArmConfig({a: 6, b: 0.05, n: 4});
-  let galaxyArm = galaxyArmConfig({a: 1.3, b: 0.5, n: 5});
+  // let galaxyArm = galaxyArmConfig({a: 1.3, b: 0.5, n: 5});
 
-  let scale = width/3;
-  let step = Math.PI / 200;
+  let armConfig = {
+    scale: width/3,
+    step: Math.PI/200,
+    numSteps: 400,
+    radius: galaxyArmConfig({a: 1.3, b: 0.5, n: 5})
+  }
 
-  // ctx.beginPath();
-  // ctx.strokeStyle = "black";
-  // ctx.moveTo(300, 300);
-  for (const stepNum of Array(400).keys()) {
-    let theta = step * stepNum;
-    let radius = galaxyArm(theta) * scale;
-    // let [coord_x, coord_y] = polarToCoords(theta, radius);
+  // Trace first arm
+  ctx.beginPath();
+  ctx.strokeStyle = "grey";
+  ctx.moveTo(centerX, centerY);
+  traceGalaxyArm(armConfig, (stepNum, theta, radius) => {
+    let [coord_x, coord_y] = polarToCoords(theta, radius);
+    ctx.lineTo(coord_x, coord_y);
+  });
+  ctx.stroke();
+
+  // Trace second arm
+  ctx.beginPath();
+  ctx.strokeStyle = "grey";
+  ctx.moveTo(centerX, centerY);
+  traceGalaxyArm(armConfig, (stepNum, theta, radius) => {
+    let [coord_x, coord_y] = polarToCoords(theta, radius * -1);
+    ctx.lineTo(coord_x, coord_y);
+  })
+  ctx.stroke();
+
+  // Draw solar systems
+  traceGalaxyArm(armConfig, (stepNum, theta, radius) => {
     for (const i of Array(20).keys()) {
       let target_radius = radius + (Math.random() * 100) - 50
       let [coord_x, coord_y] = polarToCoords(theta, target_radius);
       drawPoint(ctx, coord_x, coord_y);
     }
-    // ctx.lineTo(coord_x, coord_y);
-  }
-  // ctx.stroke();
-
-  // ctx.beginPath();
-  // ctx.strokeStyle = "black";
-  // ctx.moveTo(300, 300);
-  for (const stepNum of Array(400).keys()) {
-    let theta = step * stepNum;
-    let radius = galaxyArm(theta) * scale * -1;
-    for (const i of Array(20).keys()) {
-      let target_radius = radius + (Math.random() * 100) - 50
-      let [coord_x, coord_y] = polarToCoords(theta, target_radius);
-      drawPoint(ctx, coord_x, coord_y);
-    }
-    // let [coord_x, coord_y] = polarToCoords(theta, radius);
-    // drawPoint(ctx, coord_x, coord_y)
-    // ctx.lineTo(coord_x, coord_y);
-  }
-  // ctx.stroke();
+  })
 }
